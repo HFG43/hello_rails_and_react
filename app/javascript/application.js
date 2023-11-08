8b4862b4ab4abf44d0d4fb49b46e63cf3a0842b0
HFG43 <hernanguemes@hotmail.com>
2023-11-08 09:56:54 -0300

    Feat: generate App, Greeting components & Store to manage state

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+// function App() {
+//   return (<h1>Hello World!</h1>);
+// }
 
 ReactDOM.render(
   <App/>,
